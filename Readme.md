12cab42323a05773f26ae691504def1e45bf8ced
BerserKess <kesleyrainery.21@gmail.com>
2022-11-14 14:03:23 -0300

    alterações e readme

---
+++ Readme.md
@@ -0,0 +1,35 @@
+<h1 align="center"> Projeto AluraTube (Imersão React) ⚽</h1>
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+<div>
+<h2 align="center"> Preview </h2>
+  <p align="center"><img height="50%" width="50%" src="./.github/preview01.jpg"></p>
+  <p align="center"> <img height="50%" width="50%"  src="./.github/preview02.jpg"> </p>
+  
+</div>
+    
+   ## 🚀 Tecnologias
+   <p>Esse projeto foi desenvolvido com a seguintes tecnologia:<br><br>
+    <img  alt="React" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg">
+    </p>
+ 
+## 💻 Projeto
+<div>
+  <ul>
+    <li><p>Projeto idealizado pela <i><a href="https://github.com/alura-cursos">@Alura Cursos</a></i>🚀 </p></li>
+    <li>Autor : <a href="https://github.com/BerserKess">Kesley Rainery</a></p></li>
+    <li><p>Proposta: Construir um site para visualização de videos, parecido com o Youtube</p></li>
+    
+   </ul>
+   
+</div>
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,71 +1,77 @@
 import React from "react";
 import { StyledRegisterVideo } from "./style";
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
 //Custom Hook
 function useForm() {
-    const [values, setValues] = React.useState("");
-    return{
-        values,
-        handleChange: (evento) => {
-            const value = evento.target.value;
-            const name = evento.target.name
-            setValues({
-              ...values,
-              [name]: value,
-            });
-          },
-          clearForm(){
-            setValues({})
-          }
-    }
+  const [values, setValues] = React.useState("");
+  return {
+    values,
+    handleChange: (evento) => {
+      const value = evento.target.value;
+      const name = evento.target.name;
+      setValues({
+        ...values,
+        [name]: value,
+      });
+    },
+    clearForm() {
+      setValues({});
+    },
+  };
 }
-const PROJECT_URL = "https://orgpddrveviplfaeiokr.supabase.co"
-const PROJETC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yZ3BkZHJ2ZXZpcGxmYWVpb2tyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxNzY5NjIsImV4cCI6MTk4Mzc1Mjk2Mn0.4IsBNSF2VypKaBGbu1FCC8I3TYXPtn5jcszBCYG8-jc"
-const supabase = createClient(PROJECT_URL, PROJETC_KEY)
-
+const PROJECT_URL = "https://orgpddrveviplfaeiokr.supabase.co";
+const PROJETC_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yZ3BkZHJ2ZXZpcGxmYWVpb2tyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxNzY5NjIsImV4cCI6MTk4Mzc1Mjk2Mn0.4IsBNSF2VypKaBGbu1FCC8I3TYXPtn5jcszBCYG8-jc";
+const supabase = createClient(PROJECT_URL, PROJETC_KEY);
 
 function getThumbnail(url) {
   return `https://img.youtube.com/vi/${url.split("v=")[1]}/hqdefault.jpg`;
 }
 
-
 export default function RegisterVideo() {
   const [formVisivel, setFormVisivel] = React.useState(false);
   const formCadastro = useForm({
-    initialValues: { titulo: "Frost punk", url: "https://www.youtube.com/watch?v=QsqatJxAUtk" }
-});
+    initialValues: {
+      titulo: "Frost punk",
+      url: "https://www.youtube.com/watch?v=QsqatJxAUtk",
+    },
+  });
 
   return (
     <StyledRegisterVideo>
       <button
         className="add-video"
         onClick={() => {
-          setFormVisivel(true)
+          setFormVisivel(true);
         }}
       >
         +
       </button>
       {formVisivel ? ( // abre e fecha o modal
-        <form onSubmit={(evento)=>{
-            evento.preventDefault();            
-            supabase.from("video").insert({
-              title: formCadastro.values.titulo,
-              url: formCadastro.values.url,
-              thumb: getThumbnail(formCadastro.values.url),
-              playlist: "games",
-           })
-           .then((oqueveio) => {
-              console.log(oqueveio);
-           })
-           .catch((err) => {
-              console.log(err);
-           })
-           setFormVisivel(false);
-
-        }}>
+        <form
+          onSubmit={(evento) => {
+            evento.preventDefault();
+            supabase
+              .from("video")
+              .insert({
+                title: formCadastro.values.titulo,
+                url: formCadastro.values.url,
+                thumb: getThumbnail(formCadastro.values.url),
+                playlist: "music",
+              })
+              .then((oqueveio) => {
+                console.log(oqueveio);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+            setFormVisivel(false);
+          }}
+        >
           <div>
-            <button type="button"
+            <button
+              type="button"
               className="close-modal"
               onClick={() => {
                 setFormVisivel(false);
@@ -79,9 +85,12 @@ export default function RegisterVideo() {
               value={formCadastro.values.titulo}
               onChange={formCadastro.handleChange}
             />
-            <input placeholder="URL"
-            name="url"
-            value={formCadastro.values.url} onChange={formCadastro.handleChange} />
+            <input
+              placeholder="URL"
+              name="url"
+              value={formCadastro.values.url}
+              onChange={formCadastro.handleChange}
+            />
             <button type="submit">Cadastrar</button>
           </div>
         </form>
